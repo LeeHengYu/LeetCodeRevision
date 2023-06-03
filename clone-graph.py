3d4864362b053a4658401bfb415bdcb34421af96
LeeHengYu <132736620+LeeHengYu@users.noreply.github.com>
2023-06-04 06:45:03 +0800

    Add second BFS solution to clone-graph in C++

--- clone-graph.py
+++ clone-graph.py
@@ -6,7 +6,7 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-class Solution:
+class Solution1: # DFS recursion in Python 3
     def cloneGraph(self, node: 'Node') -> 'Node':
         mp = {}
         if not node: return None
@@ -24,3 +24,41 @@ class Solution:
             return copy
 
         return clone(node)
+    
+# Credit: NeetCode
+
+class Solution2 { # BFS in C++
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+
+        queue<Node*> q; //processing q
+        q.push(node);
+
+        Node* clone = new Node(node->val);
+        mp[node] = clone; //map old node to new node in the hash table
+
+        while(!q.empty()){
+            Node* curr = q.front();
+            q.pop();
+
+            //process all the neighbors of CURR
+            for (int i=0; i<curr->neighbors.size(); ++i){
+                Node* currnei = curr->neighbors[i];
+                
+                if (mp.find(currnei)==mp.end()){
+                    mp[currnei] = new Node (currnei->val);
+                    q.push(currnei);
+                }
+
+                mp[curr]->neighbors.push_back(mp[currnei]);
+            }
+        }
+
+        return mp[node];
+    }
+
+private:
+    unordered_map<Node*, Node*> mp;
+};
+
