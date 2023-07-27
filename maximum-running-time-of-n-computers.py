8d2d8ad5e7d6f3f2fa89561252f8b968643d6670
hengyulee <hengyuli901229@gmail.com>
2023-07-27 09:29:09 +0800

    add 727

---
+++ maximum-running-time-of-n-computers.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        def check(cap):
+            return sum(min(cap,b) for b in batteries)>=n*cap
+            # If one battery can sustain cap mins, then use it throughout
+            # Else, combine with other batteries for the whole period
+        l, r = 1, 10**14
+        res = 0
+        while l<=r:
+            m=(l+r)//2
+            if check(m):
+                res = m
+                l=m+1
+            else:
+                r=m-1
+        return res
