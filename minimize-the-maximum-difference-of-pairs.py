2b5e78ed10504238c000ade9ce14a42ffceea827
hengyulee <hengyuli901229@gmail.com>
2023-08-09 10:18:59 +0800

    updateto Neetcode sol

--- minimize-the-maximum-difference-of-pairs.py
+++ minimize-the-maximum-difference-of-pairs.py
@@ -1,19 +1,29 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
+        if not p: return 0
+        
         nums.sort()
         n = len(nums)
-        l,r=0,nums[-1]-nums[0]
-        while l<r:
-            mid = (l+r)//2
-            k,i=0,1 # current pair, index reached
+        l, r = 0, nums[-1]-nums[0]
+        
+        def isValid(m):
+            i, cnt = 1, 0
             while i<n:
-                if nums[i]-nums[i-1]<=mid:
-                    k+=1
+                if nums[i]-nums[i-1]<=m:
+                    i+=2
+                    cnt+=1
+                else:
                     i+=1
-                i+=1 # jump to new pair
-            if k>=p: # valid, if total number of pairs with the difference <= mid is more than p
-                # reduce the target res, but mid can also be the ans
-                r=mid # real difference can be smaller
+                if cnt==p:
+                    return True
+            return False
+        res = r 
+        while l<=r:
+            m = (l+r)//2
+            if isValid(m):
+                res = m
+                r = m-1
             else:
-                l=mid+1
-        return min(l, r)
+                l = m+1
+            
+        return res
