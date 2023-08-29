88245b0b3d441dd1abaf996f26a75f4d745cec9e
hengyulee <hengyuli901229@gmail.com>
2023-08-29 12:15:55 +0800

    update

--- minimum-penalty-for-a-shop.py
+++ minimum-penalty-for-a-shop.py
@@ -2,14 +2,11 @@ class Solution:
     def bestClosingTime(self, customers: str) -> int:
         N = len(customers)
         Y, N = customers.count('Y'), 0
-        min_pen = Y+N
+        min_pen = temp = Y+N
         res = 0
         for i, c in enumerate(customers):
-            if c =='Y':
-                Y-=1
-            elif c=='N':
-                N+=1
-            if Y+N < min_pen:
-                min_pen = Y+N
+            temp += (1 if c=='N' else -1)
+            if temp < min_pen:
+                min_pen = temp
                 res = i+1
         return res
