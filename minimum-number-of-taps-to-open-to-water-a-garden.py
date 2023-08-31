fea0857e2255d2e60601a66921a715d0c1dc3389
hengyulee <hengyuli901229@gmail.com>
2023-08-31 09:21:52 +0800

    update

--- minimum-number-of-taps-to-open-to-water-a-garden.py
+++ minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -2,13 +2,10 @@ from math import inf
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
         jumps = [0]*(n+1)
-        validStart = False
         for i, j in enumerate(ranges):
             left = max(0,i-j)
-            if left==0: validStart = True
             right = min(n,i+j)
             jumps[left] = max(jumps[left], right-left)
-        if not validStart: return -1
         
         # Jump Game II
         dp = [inf]*(n+1) # see if can reach index n
