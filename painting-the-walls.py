0e146532e1146326203a79d0d6e44ffccd2bbd46
hengyulee <hengyuli901229@gmail.com>
2023-10-14 08:49:30 +0800

    update 1014

--- painting-the-walls.py
+++ painting-the-walls.py
@@ -1,23 +1,14 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
-        # DP
+        
         n = len(cost)
-        cache = {}  # this caching the min cost incurred at that step
+        @cache
+        def dp(i, walls: int) -> int:
+            if walls<=0: return 0
+            if i>=n: return inf
 
-        def solve(i, tc, w):  # this returns the total cost
-            if (i, w) in cache:
-                return cache[(i, w)]+tc
-            if w <= 0:
-                return tc
-            if i >= n:
-                return inf
+            paid = dp(i+1, walls-1-time[i]) + cost[i]
+            skip = dp(i+1, walls)
+            return min(paid, skip)
 
-            paid = solve(i+1, tc+cost[i], w-time[i]-1)
-            free = solve(i+1, tc, w)
-
-            cache[(i, w)] = min(paid, free)-tc
-            return min(paid, free)
-
-        return solve(0, 0, n)
-
-    # Sol: https://leetcode.com/problems/painting-the-walls/solutions/3655230/line-by-line-explanation-for-dummies-like-me/
+        return dp(0,n)
