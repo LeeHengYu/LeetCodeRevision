856c158541d3522dac2356240ccafd81b0379955
hengyulee <hengyuli901229@gmail.com>
2023-12-04 16:55:53 +0800

    update 1204

--- largest-3-same-digit-number-in-string.py
+++ largest-3-same-digit-number-in-string.py
@@ -1,12 +1,8 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        s = {"000", "111", "222", "333", "444", "555", "666", "777", "888", "999"}
         n = len(num)
-        res = -1
-        for i in range(3, n+1):
-            if num[i-3:i] in s:
-                res = max(res, int(num[i-3:i]))
-
-        if res == -1: return ""
-        if res == 0: return "000"
-        return str(res)
+        res = "0"
+        for i in range(n-2):
+            if num[i]==num[i+1]==num[i+2] and int(num[i:i+3])>=int(res):
+                res = num[i:i+3]
+        return res if res!="0" else ""
