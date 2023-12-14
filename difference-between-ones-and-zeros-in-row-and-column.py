ab9a0f136e47867028170b7143040625337ccccb
hengyulee <hengyuli901229@gmail.com>
2023-12-14 12:35:44 +0800

    update 1214

--- difference-between-ones-and-zeros-in-row-and-column.py
+++ difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,15 +1,13 @@
-from collections import defaultdict
-
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
         m, n = len(grid), len(grid[0])
-        RowOnes = defaultdict(int)
-        ColOnes = defaultdict(int)
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]:
-                    RowOnes[i] += 1
-                    ColOnes[j] += 1
+        RowOnes = [0]*m
+        ColOnes = [0]*n
+        for r in range(m):
+            RowOnes[r] = sum(grid[r])
+        for c in range(n):
+            ColOnes[c] = sum([ grid[x][c] for x in range(m) ])
+            
         for i in range(m):
             for j in range(n):
                 # onesRow - zerosRow = onesRow - (n - onesRow)
