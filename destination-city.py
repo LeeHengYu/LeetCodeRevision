78de8c3487177ff97738e847948f51f7511ca5a0
hengyulee <hengyuli901229@gmail.com>
2023-12-15 19:50:29 +0800

    update 1215

--- destination-city.py
+++ destination-city.py
@@ -1,7 +1,6 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
         start = set()
-        end = set()
         for u, _ in paths:
             start.add(u)
         for _, v in paths:
