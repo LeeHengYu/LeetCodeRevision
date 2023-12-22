cb4860c32065960fc337dbefca27b77ce0913644
hengyulee <hengyuli901229@gmail.com>
2023-12-22 17:12:46 +0800

    update 1222

--- maximum-score-after-splitting-a-string.py
+++ maximum-score-after-splitting-a-string.py
@@ -1,14 +1,11 @@
-from collections import Counter
-
 class Solution:
     def maxScore(self, s: str) -> int:
-        ones = Counter(s)['1']
         left = res = 0
-        right = ones
+        right = s.count('1')
         for i in range(len(s)-1):
             if s[i] == '0':
                 left+=1
             else:
                 right-=1
             res = max(res, left+right)
-        return res  
+        return res
